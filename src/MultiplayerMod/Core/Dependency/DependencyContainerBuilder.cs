67362d4015f51077c0c436c3d7542b2c2b85cdd5
Denis Pakhorukov <denis@polycone.dev>
2024-08-25 03:22:14 +0300

    Add filtering assembly scan

--- src/MultiplayerMod/Core/Dependency/DependencyContainerBuilder.cs
+++ src/MultiplayerMod/Core/Dependency/DependencyContainerBuilder.cs
@@ -7,19 +7,21 @@ namespace MultiplayerMod.Core.Dependency;
 
 public class DependencyContainerBuilder {
 
-    private readonly List<(Assembly, string[])> assemblies = new();
+    private readonly List<(Assembly, Func<Type, bool>)> assemblies = new();
     private readonly List<Type> includeTypes = new();
     private readonly List<object> singletons = new();
 
     public event Action<DependencyContainer>? ContainerCreated;
 
-    public DependencyContainerBuilder ScanAssembly(Assembly assembly) {
-        assemblies.Add((assembly, Array.Empty<string>()));
-        return this;
-    }
+    public DependencyContainerBuilder ScanAssembly(Assembly assembly) => ScanAssembly(assembly, _ => true);
 
-    public DependencyContainerBuilder ScanAssembly(Assembly assembly, params string[] namespaces) {
-        assemblies.Add((assembly, namespaces));
+    public DependencyContainerBuilder ScanAssembly(Assembly assembly, params string[] namespaces) => ScanAssembly(
+        assembly,
+        type => namespaces.Any(name => type.Namespace?.StartsWith(name) == true)
+    );
+
+    public DependencyContainerBuilder ScanAssembly(Assembly assembly, Func<Type, bool> predicate) {
+        assemblies.Add((assembly, predicate));
         return this;
     }
 
@@ -39,15 +41,7 @@ public class DependencyContainerBuilder {
         var container = new DependencyContainer();
         container.RegisterSingleton(container);
         singletons.ForEach(it => container.RegisterSingleton(it));
-        var types = assemblies.SelectMany(
-                it => {
-                    var (assembly, namespaces) = it;
-                    var types = assembly.DefinedTypes;
-                    if (namespaces.Length > 0)
-                        types = types.Where(type => namespaces.Any(name => type.Namespace!.StartsWith(name)));
-                    return types;
-                }
-            )
+        var types = assemblies.SelectMany(it => it.Item1.DefinedTypes.Where(it.Item2))
             .Where(it => it.GetCustomAttribute<DependencyAttribute>() != null)
             .Union(includeTypes);
         foreach (var type in types) {
