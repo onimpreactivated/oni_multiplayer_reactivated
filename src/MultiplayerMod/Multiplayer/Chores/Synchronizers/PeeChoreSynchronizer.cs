46d5771ac82c839c14417d4f19b32dad70ae1fe1
Denis Pakhorukov <denis@polycone.dev>
2024-08-27 00:10:31 +0300

    Add PeeChore sync

---
+++ src/MultiplayerMod/Multiplayer/Chores/Synchronizers/PeeChoreSynchronizer.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using MultiplayerMod.Core.Dependency;
+using MultiplayerMod.Multiplayer.StateMachines.Configuration;
+using MultiplayerMod.Multiplayer.StateMachines.Configuration.Configurers;
+
+namespace MultiplayerMod.Multiplayer.Chores.Synchronizers;
+
+[Dependency, UsedImplicitly]
+public class PeeChoreSynchronizer : ChoreSynchronizer<PeeChore, PeeChore.States, PeeChore.StatesInstance> {
+
+    protected override void Configure(IStateMachineRootConfigurer<PeeChore.States, PeeChore.StatesInstance, PeeChore, object> root) {
+        root.Inline(new StateMachineConfigurerDsl<PeeChoreMonitor, PeeChoreMonitor.Instance>(monitor => {
+            monitor.PreConfigure(MultiplayerMode.Client, pre => pre.Suppress(() => pre.StateMachine.pee.ToggleChore(null, null)));
+        }));
+    }
+
+}
